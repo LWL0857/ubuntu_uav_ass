e46af9ecd806242eeea2079abe2dc7853c066ed1
L857 <1813285597@163.com>
2023-07-26 03:47:35 +0000

    SUCCESS

--- uav_base/launch/uav.launch.py
+++ uav_base/launch/uav.launch.py
@@ -1,20 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-# Copyright (c) 2022, www.guyuehome.com
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import os
 import launch
 
@@ -29,10 +12,6 @@ def generate_launch_description():
     #                                          description='Use simulation clock if true')
     port_name_arg = DeclareLaunchArgument('port_name', default_value='ttyS3',
                                          description='usb bus name, e.g. ttyS3')
-  #  auto_stop_on_arg = DeclareLaunchArgument('auto_stop_on', default_value='false',
-  #                                       description='auto stop if no cmd received, true or false')
-#    use_imu_arg = DeclareLaunchArgument('use_imu', default_value='false',
-#                                         description='if has imu sensor to drive')
     use_mocap_arg= DeclareLaunchArgument("use_mocap",default_value='false',
                                         description='if has mocap use')
     use_uwb_arg = DeclareLaunchArgument('use_uwb', default_value='false',                                             
